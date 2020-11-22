33a6ee869e068df6347f95f5d5cd0f7592a7f42e
Des7r0 <akamaru1337@gmail.com>
2020-11-22 16:45:19 +0300

    Сложный урок

---
+++ script/script.js
@@ -0,0 +1,8 @@
+let num = 266219; 
+let numStr = num.toString(); 
+console.log(numStr[0] * numStr[1] * numStr[2] * numStr[3] * numStr[4] * numStr[5]); 
+let result = 1296;
+result **= 3;
+console.log(result);
+let resStr = result.toString();
+alert(resStr.substr(0,2));
