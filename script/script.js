2e37fefc28401948ddf9d2a7dac78af5c972f1d7
Des7r0 <akamaru1337@gmail.com>
2020-11-23 19:12:20 +0300

    Улучишил код

--- script/script.js
+++ script/script.js
@@ -2,8 +2,7 @@ let num = 266219;
 let numToStr = num.toString(); // преобразовали число в строку
 let numSplit = numToStr.split(''); // разбили строку на массив
 const reducer = (accumulator, currentValue) => accumulator * currentValue; // применили функцию reducer
-console.log(numSplit.reduce(reducer));
-let result = 1296;
+let result = numSplit.reduce(reducer)
 result **= 3;
 console.log(result);
 let resToStr = result.toString();
