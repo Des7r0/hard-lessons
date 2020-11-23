ab9a1b34cacb3c29bd4b327f2c55f70f542eb68d
Des7r0 <akamaru1337@gmail.com>
2020-11-23 14:33:47 +0300

    Исправил усложнённое задание

--- script/script.js
+++ script/script.js
@@ -6,5 +6,5 @@ console.log(numSplit.reduce(reducer));
 let result = 1296;
 result **= 3;
 console.log(result);
-let resStr = result.toString();
-alert(resStr.substr(0,2));
+let resToStr = result.toString();
+alert(resToStr.substr(0,2));
